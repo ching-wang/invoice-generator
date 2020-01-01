8b9d86234eba925226c37c7d510fea9dd0a08e61
Qing <chingwang.qw@gmail.com>
2020-01-01 18:47:23 +0000

    Tidy up layout and editing

--- backend/app/controllers/invoices_controller.rb
+++ backend/app/controllers/invoices_controller.rb
@@ -1,18 +1,23 @@
 class InvoicesController < ApplicationController
   def index
     invoices = Invoice.all
-    render json: invoices, include: [:user, :buyer]
+    render json: invoices, include: [:user, :buyer, :workItems]
   end
 
   def show
     invoice = Invoice.find_by(id: params[:id])
-    render json: invoice, include: [:user, :buyer]
+    render json: invoice, include: [:user, :buyer, :workItems]
   end
 
   def update
-    invoice = Invoice.find_by(id: params[:id])
-    invoice.update
-    invoice.save
-    render json: invoice, include: [:user, :buyer]
+    invoice = Invoice.find(params[:id])
+    invoice.update(invoice_params)
+    render json: invoice, include: [:user, :buyer, :workItems]
+  end
+
+  private
+
+  def invoice_params
+    params.require(:invoice).permit(:invoiceNumber)
   end
 end
--- frontend/src/selectors.js
+++ frontend/src/selectors.js
@@ -10,12 +10,9 @@ const invoiceNumLi = document.createElement("li");
 const invContainer = document.querySelector("#invoice-container");
 
 const showInvDetail = document.querySelector("#show-inv-detail");
-showInvDetail.classList.add("card");
 
 const logoImg = document.createElement("img");
 logoImg.classList.add("card-img-top");
-logoImg.style.width = "30%";
-logoImg.style.height = "auto";
 
 const invNum = document.createElement("p");
 invNum.id = "inv-num";
@@ -25,10 +22,10 @@ const userName = document.createElement("h5");
 const userAddr = document.createElement("p");
 
 const userCardBody = document.createElement("div");
+userCardBody.classList.add("card-body");
 
 const userCard = document.createElement("div");
 userCard.classList.add("card");
-userCard.style.width = "18rem";
 
 const buyerName = document.createElement("h5");
 const buyerAddr = document.createElement("p");
@@ -36,4 +33,3 @@ const buyerCardBody = document.createElement("div");
 
 const buyerCard = document.createElement("div");
 buyerCard.classList.add("card");
-buyerCard.style.width = "18rem";
